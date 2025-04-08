3669d23de684099d31c73ff0e4c8aadaf5bdb98c
amine karime <contact.amine.karime@gmail.com>
2025-04-08 18:50:36 +0100

    FIRST WORKING VERSION
    Fixed the jiggling  servos by using a 9V battery, and updated the code / it has no timing yet, just the base logic.

--- Sorting Station/MAIN.ino
+++ Sorting Station/MAIN.ino
@@ -6,46 +6,30 @@
 Servo servo1, servo2, servo3;
 Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_600MS, TCS34725_GAIN_1X);
 
-// KY-016 RGB LED Pins
+// RGB LED Pins
 #define RED_LED_PIN 11
 #define GREEN_LED_PIN 9
 #define BLUE_LED_PIN 10
 
-// Color thresholds
+// Color thresholds (keep your values)
 #define RED_THRESHOLD   12500
 #define GREEN_THRESHOLD 11500
 #define WHITE_THRESHOLD 37000
 
-// Timing
-#define RED_DELAY    15000  // 15 seconds
-#define GREEN_DELAY  24000  // 24 seconds
-#define WHITE_DELAY  5000   // 5 seconds
-
-// Servo configuration
-#define POS_PUSH 110
-#define POS_IDLE 180
-#define SERVO_HOLD 1000  // 1 second push
-
 // Relay Pin
 #define RELAY_PIN 7
 
-// State machine
-enum SystemState { IDLE, CONVEYOR_MOVING, PUSHING };
-SystemState currentState = IDLE;
+// Timing
+#define CHECK_INTERVAL 2000  // 2 seconds between readings
 
-unsigned long actionStartTime;
-uint8_t activeColor = 0;  // 1=Red, 2=Green, 3=White
+unsigned long lastCheck = 0;
 
 void setup() {
-  // Initialize RGB LED
   pinMode(RED_LED_PIN, OUTPUT);
   pinMode(GREEN_LED_PIN, OUTPUT);
   pinMode(BLUE_LED_PIN, OUTPUT);
-  setLED(0, 0, 0);  // Start with LED off
-
-  // Initialize other components
   pinMode(RELAY_PIN, OUTPUT);
-  digitalWrite(RELAY_PIN, LOW);
+  setLED(0, 0, 0);
 
   servo1.attach(3);
   servo2.attach(5);
@@ -54,100 +38,65 @@ void setup() {
 
   Serial.begin(9600);
   if (!tcs.begin()) {
-    Serial.println("Color sensor error!");
+    Serial.println("Sensor error!");
     while(1);
   }
 }
 
 void loop() {
-  switch(currentState) {
-    case IDLE:
-      checkForBlock();
-      break;
-
-    case CONVEYOR_MOVING:
-      handleConveyor();
-      break;
-
-    case PUSHING:
-      handlePushing();
-      break;
+  if (millis() - lastCheck >= CHECK_INTERVAL) {
+    checkColor();
+    lastCheck = millis();
   }
 }
 
-void checkForBlock() {
+void checkColor() {
   uint16_t r, g, b, c;
   tcs.getRawData(&r, &g, &b, &c);
-
+  
   if (r > RED_THRESHOLD && r > g && r > b) {
-    startProcessing(1, RED_DELAY);
-    setLED(255, 0, 0);  // Red
-  }
+    processColor(1, 255, 0, 0);  // Red
+  } 
   else if (g > GREEN_THRESHOLD && g > r && g > b) {
-    startProcessing(2, GREEN_DELAY);
-    setLED(0, 255, 0);  // Green
-  }
+    processColor(2, 0, 255, 0);  // Green
+  } 
   else if (r > WHITE_THRESHOLD && g > WHITE_THRESHOLD && b > WHITE_THRESHOLD) {
-    startProcessing(3, WHITE_DELAY);
-    setLED(255, 255, 255);  // White
+    processColor(3, 255, 255, 255);  // White
+  }
+  else {
+    Serial.println("Unknown color");
+    resetServos();
   }
 }
 
-void startProcessing(uint8_t color, unsigned long delay) {
-  activeColor = color;
-  currentState = CONVEYOR_MOVING;
-  actionStartTime = millis();
+void processColor(uint8_t color, int ledR, int ledG, int ledB) {
+  setLED(ledR, ledG, ledB);
   digitalWrite(RELAY_PIN, HIGH);
-}
+  delay(1000);  // Relay ON time
 
-void handleConveyor() {
-  unsigned long elapsed = millis() - actionStartTime;
-  unsigned long requiredDelay = 0;
-
-  switch(activeColor) {
-    case 1: requiredDelay = RED_DELAY; break;
-    case 2: requiredDelay = GREEN_DELAY; break;
-    case 3: requiredDelay = WHITE_DELAY; break;
+  // Move servo
+  switch(color) {
+    case 1: servo1.write(90); break;
+    case 2: servo2.write(90); break;
+    case 3: servo3.write(90); break;
   }
-
-  if (elapsed >= requiredDelay) {
-    currentState = PUSHING;
-    actionStartTime = millis();
-    activateServo();
-  }
-}
-
-void activateServo() {
-  switch(activeColor) {
-    case 1: servo1.write(POS_PUSH); break;
-    case 2: servo2.write(POS_PUSH); break;
-    case 3: servo3.write(POS_PUSH); break;
-  }
-}
-
-void handlePushing() {
-  if (millis() - actionStartTime >= SERVO_HOLD) {
-    resetSystem();
-  }
-}
-
-void resetSystem() {
-  digitalWrite(RELAY_PIN, LOW);
-  setLED(0, 0, 0);  // Turn off LED
+  
+  delay(2000);  // Hold servo position
+  
+  // Reset
   resetServos();
-  currentState = IDLE;
-  activeColor = 0;
+  digitalWrite(RELAY_PIN, LOW);
+  setLED(0, 0, 0);
 }
 
-// LED control function
+void resetServos() {
+  servo1.write(180);
+  servo2.write(180);
+  servo3.write(180);
+}
+
 void setLED(int r, int g, int b) {
   analogWrite(RED_LED_PIN, r);
   analogWrite(GREEN_LED_PIN, g);
   analogWrite(BLUE_LED_PIN, b);
-}
-
-void resetServos() {
-  servo1.write(POS_IDLE);
-  servo2.write(POS_IDLE);
-  servo3.write(POS_IDLE);
 }
